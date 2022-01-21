f418342bdff53db2a222f7b0abb31b3bb48ace86
Your Name <you@example.com>
2022-01-21 16:54:24 +0200

    completed Client class

--- src/main/java/com/hillel/cinema/model/Client.java
+++ src/main/java/com/hillel/cinema/model/Client.java
@@ -1,4 +1,30 @@
 package com.hillel.cinema.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "clients")
 public class Client {
+    @Id
+    @GeneratedValue
+    private Long clientId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Date dateOfBirth;
+    private int cartNumber;
+    private int number;
+
+
 }
