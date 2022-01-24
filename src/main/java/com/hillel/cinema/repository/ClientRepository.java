7be0542b8cb3d30fd41453d8b6945ae16e11916b
vladivo1 <vla.ivolga@gmail.com>
2022-01-24 14:23:53 +0200

    added service and controller for Client, small changes in entities

--- src/main/java/com/hillel/cinema/repository/ClientRepository.java
+++ src/main/java/com/hillel/cinema/repository/ClientRepository.java
@@ -5,7 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
-    public Client findByEmail(String email);
-    public Client findByPhoneNumber(String phoneNumber);
-
 }
